ebf65951e63b3147c6210ca411c77b1c37af1a0e
Kaal <79936805+Kaalwy@users.noreply.github.com>
2024-10-25 16:24:58 -0300

    update

--- README.md
+++ README.md
@@ -1,13 +1,20 @@
 # MeiBot
 
-MeiBot is a simple yet powerful Discord bot designed to enhance your Discord server's experience. It offers various features, including music playback, dice rolling, moderation tools, and more. If you'd like to contribute or collaborate on this project, feel free to reach out to me on Discord with the username `kaal_wy`.
+MeiBot is a simple yet powerful Discord bot designed to enhance your Discord server's experience. It offers various features, including music playback, dice rolling, RPG character creation, moderation tools, and more. If you'd like to contribute or collaborate on this project, feel free to reach out to me on Discord with the username `kaal_wy`.
 
 ## Features
 
 - **Music Playback**: Stream music directly in voice channels from YouTube and local files.
 - **Dice Rolling**: Roll various types of dice for gaming purposes with informative and fun responses.
+- **RPG Features**: Create, customize, and manage RPG characters through interactive commands.
 - **Moderation Tools**: Mute, unmute, kick, or ban users with documented commands.
 - **Custom Command Prefix**: Use a custom prefix or mention the bot to trigger commands.
+
+## RPG Features
+
+- **RPG Character Creation**: Personalize your character through an interactive question system that adjusts attributes and abilities based on your choices, including classes and unique traits.
+- **Character Status Check**: Check your character's stats and abilities anytime with a quick command.
+- **Interactive Quiz for Skills**: Gain unique skills for your character based on responses to an interactive quiz, enriching the RPG experience.
 
 ## Getting Started
 
@@ -21,55 +28,62 @@ Ensure you have Python 3.8 or higher installed. You can download it from [Python
 
 1. **Clone the repository**:
 
- ```bash
- git clone https://github.com/Kaalwy/MeiBot.git
- cd MeiBot
- ```
+    ```bash
+    git clone https://github.com/Kaalwy/MeiBot.git
+    cd MeiBot
+    ```
 
 2. **.env required**:
 
-Create a .env file in the root of your project with the following contents:
+    Create a .env file in the root of your project with the following contents:
 
-YOUR_TOKEN=your_discord_bot_token_here
+    ```
+    YOUR_TOKEN=your_discord_bot_token_here
+    ```
 
 3. **Install the requirements**:
-```bash
-pip install -r requirements.txt
-```
+
+    ```bash
+    pip install -r requirements.txt
+    ```
 
 4. **Test it!**
-```bash
-python bot.py
-```
 
-### Usage
+    ```bash
+    python bot.py
+    ```
+
+## Usage
 
 Once your bot is up and running, you can invite it to your Discord server using the OAuth2 URL generated from the Discord Developer Portal. Use the following command prefixes to interact with MeiBot:
-```
-    Music Commands:
-        .join - Joins your voice channel.
-        .leave - Leaves the voice channel.
-        .play <URL_or_MP3> - Plays music from a URL or local file.
-        .stop - Stops the music.
-        .skip - Skips to the next song in the queue.
 
-    Dice Commands:
-        .r <command> - Rolls dice (e.g., .r d20, .roll 2d6+3).
+### Music Commands:
+- `.join` - Joins your voice channel.
+- `.leave` - Leaves the voice channel.
+- `.play <URL_or_MP3>` - Plays music from a URL or local file.
+- `.stop` - Stops the music.
+- `.skip` - Skips to the next song in the queue.
 
-    Moderation Commands:
-        .mute @user <duration> - Silences a user for a specified duration.
-        .unmute @user - Removes the mute from a user.
-        .ban @user <reason> - Bans a user.
-        .kick @user <reason> - Kicks a user from the server.
-```
+### Dice Commands:
+- `.r <command>` - Rolls dice (e.g., .r d20, .roll 2d6+3).
 
-### Contributing
+### RPG Commands:
+- `.caketale` - Initiates the character creation process for Caketale, with a question-based system that adjusts attributes and skills.
+- `.status` - Displays the current status of your character, showing attributes and skills for easy reference during adventures.
+
+### Moderation Commands:
+- `.mute @user <duration>` - Silences a user for a specified duration.
+- `.unmute @user` - Removes the mute from a user.
+- `.ban @user <reason>` - Bans a user.
+- `.kick @user <reason>` - Kicks a user from the server.
+
+## Contributing
 
 Contributions are welcome! If you'd like to work on MeiBot, please follow these steps:
-```
+
 - Fork the repository.
 - Create a new branch for your feature or fix.
 - Make your changes.
 - Submit a pull request with a clear description of your changes.
-```
-or you can also call me by email or discord "kaalwy@proton.me"
+
+Alternatively, you can contact me via email or Discord at "kaalwy@proton.me".
--- commands/cmd.py
+++ commands/cmd.py
@@ -32,40 +32,45 @@ bot = commands.Bot(command_prefix=".", intents=intents)
 
 def setup_general_commands(bot):
     async def send_help_message(ctx_or_interaction):
-        help_message = """
-        ```md
-# Todos os comandos da Mei por categoria:
------------------------------------
-
-* O prefixo para usar esse bot é o "." todos os comandos abaixo usam essa regra para serem evocados.
-
-## Moderation:
-- `kick (user) (reason)`: expulsa um usuário do server.
-- `ban (user) (reason)`: bane um usuário do server.
-- `clear (amount)`: função de chat clear ele limpa a quantidade de mensagens que você especificar no amount.
-- `ticket`: cria uma thread privada para o usuário que executou o comando.
-- `mute (user) (tempo de mute) (reason)`: muta um usuário e adiciona um cargo de mute na pessoa, coleta informações do usuário.
-- `unmute (user)`: tira o cargo de mute e o mute do usuário.
-
-## Music and call commands (BETA):
-- `join`: entra no seu canal de voz.
-- `leave`: sai do canal de voz.
-- `play (link)`: toca uma musica.
-- `skip`: skipa uma musica e toca a proxima na queue.
-- `stop`: para todas as musicas da lista.
-- `queue`: mostra a lista de musicas a serem tocadas.
-
-## Fun:
-- `oi`: cumprimenta o bot!
-- `r (quantidade)d(tipo do dado)`: exemplo `.r d20` rola um dado de 20 faces, `.r 2d10+6` rola dois dados de 10 faces + 6 de modificador.
-        ```
-        """
-        if isinstance(ctx_or_interaction, commands.Context):
-            await ctx_or_interaction.send(help_message)
-        elif isinstance(ctx_or_interaction, discord.Interaction):
-            await ctx_or_interaction.response.send_message(help_message)
-        elif isinstance(ctx_or_interaction, discord.TextChannel):
-            await ctx_or_interaction.send(help_message)
+        categories = {
+            "⚔️ Moderação": """
+            `kick (user) (reason)`: Expulsa um usuário do server.
+            `ban (user) (reason)`: Bane um usuário do server.
+            `clear (amount)`: Limpa a quantidade especificada de mensagens.
+            `ticket`: Cria uma thread privada para o usuário.
+            `mute (user) (tempo) (reason)`: Muta um usuário com um cargo de mute.
+            `unmute (user)`: Remove o mute do usuário.
+            """,
+            "🎶 Música e Chamadas (BETA)": """
+            `join`: Entra no seu canal de voz.
+            `leave`: Sai do canal de voz.
+            `play (link)`: Toca uma música a partir de um link.
+            `skip`: Skipa para a próxima música na fila.
+            `stop`: Para todas as músicas na lista.
+            `queue`: Mostra a lista de músicas.
+            """,
+            "🎲 Diversão": """
+            `oi`: O bot te cumprimenta!
+            `r (quantidade)d(tipo do dado)`: Rola dados para jogos (ex: `.r d20` ou `.r 2d10+6`).
+            """,
+            "🍰 RPG - Caketale": """
+            `caketale`: Inicia a aventura no mundo do Caketale.
+            `status`: Mostra o status do personagem no RPG Caketale.
+            """
+        }
+        
+        for category, description in categories.items():
+            embed = discord.Embed(
+                title=category,
+                description=description,
+                color=discord.Color.red()
+            )
+            if isinstance(ctx_or_interaction, commands.Context):
+                await ctx_or_interaction.send(embed=embed)
+            elif isinstance(ctx_or_interaction, discord.Interaction):
+                await ctx_or_interaction.response.send_message(embed=embed)
+            elif isinstance(ctx_or_interaction, discord.TextChannel):
+                await ctx_or_interaction.send(embed=embed)
 
     @bot.command(name="ajuda")
     async def ajuda(ctx):
@@ -80,7 +85,6 @@ def setup_general_commands(bot):
         if message.author == bot.user:
             return
 
-        # Verifica se a mensagem contém apenas a menção ao bot
         if len(message.mentions) == 1 and message.mentions[0] == bot.user and message.content.strip() == f"<@{bot.user.id}>":
             await message.add_reaction('❓')
             await send_help_message(message.channel)
@@ -97,44 +101,80 @@ def setup_general_commands(bot):
             greeting = "Boa tarde"
         else:
             greeting = "Boa noite"
-        
-        await ctx.send(f"{greeting}, {ctx.author.mention}.")
+
+        embed = discord.Embed(
+            title="👋 Cumprimento",
+            description=f"{greeting}, {ctx.author.mention}!",
+            color=discord.Color.blue()
+        )
+        await ctx.send(embed=embed)
 
     @bot.command(name="oii", aliases=["hii", "hewwo", "hew", "heya", "oiiee", "oie", "oiie"])
-    async def hello(ctx):
+    async def hello_gif(ctx):
         current_hour = datetime.now().hour
 
         if 0 <= current_hour < 12:
-            greetings = ["https://tenor.com/view/wisteriahacks-stelle-good-morning-hsr-honkai-star-rail-gif-7894856137701322105", "https://tenor.com/view/falin-touden-falin-falin-dunmeshi-falin-dungeon-meshi-falin-delicious-in-dungeon-gif-13321700084531761987", "https://tenor.com/view/stelle-hsr-honkai-star-rail-good-morning-picmix-gif-14971906056087973106", "https://tenor.com/view/kafka-honkai-project-sekai-vocaloid-alex-gif-8757130874960721563", "https://tenor.com/view/ado-ado-gif-ado-singer-ado-good-morning-good-morning-gif-5400724748085991472", "https://tenor.com/view/marcille-dunmeshi-marcille-dungeon-meshi-marcille-delicious-in-dungeon-marcille-delicious-in-dungeon-picmix-gif-1241109772456880915", "https://tenor.com/view/ado-readymade-merry-readymade-goodmorning-ado-utaite-gif-15086115743040454412", "https://tenor.com/view/good-morning-good-morning-gif-blue-miku-hatsune-miku-gif-986122290921543599", "https://tenor.com/view/dungeon-meshi-delicious-in-dungeon-good-morning-good-morning-gif-marcille-gif-1782209361987178446", "https://tenor.com/view/good-morning-phoenix-wright-gif-6282626786686229982"]
+            greetings = ["https://tenor.com/view/wisteriahacks-stelle-good-morning-hsr-honkai-star-rail-gif-7894856137701322105", "https://tenor.com/view/falin-touden-falin-falin-dunmeshi-falin-dungeon-meshi-falin-delicious-in-dungeon-gif-13321700084531761987"]
         elif 12 <= current_hour < 18:
-            greetings = ["https://tenor.com/view/leah-pookie-bear-sanae-kochiya-touhou-gif-5920970219019213138", "https://tenor.com/view/good-afternoon-afternoon-dazai-dazai-osamu-bsd-gif-1103807297647988307"]
+            greetings = ["https://tenor.com/view/leah-pookie-bear-sanae-kochiya-touhou-gif-5920970219019213138"]
         else:
-            greetings = ["https://tenor.com/view/ado-giragira-gira-gira-good-night-goodnight-gif-5423733055869444136", "https://tenor.com/view/mithrun-dungeon-meshi-dunmeshi-good-night-goodnight-gif-13050234819842792359", "https://tenor.com/view/rei-ayanami-rei-ayanami-evangelion-nge-gif-11364400796140035367", "https://tenor.com/view/dungeon-meshi-delicious-in-dungeon-dungeon-meshi-picmix-dungeon-meshi-goodnight-good-night-images-gif-2661338799817113031", "https://tenor.com/view/furina-goodnight-furina-genshin-genshin-impact-furina-good-night-gif-7110255517157230256"]
-        
-        greeting = random.choice(greetings)
+            greetings = ["https://tenor.com/view/ado-giragira-gira-gira-good-night-goodnight-gif-5423733055869444136"]
 
-        await ctx.send(greeting)
+        greeting_gif = random.choice(greetings)
+        
+        embed = discord.Embed(
+            title="👋 Cumprimento",
+            description=f"{ctx.author.mention}, aqui vai um cumprimento especial!",
+            color=discord.Color.purple()
+        )
+        embed.set_image(url=greeting_gif)
+        
+        await ctx.send(embed=embed)
 
     @bot.command(name="ticket")
     async def ticket(ctx):
         thread = await ctx.channel.create_thread(name=f"Ticket-{ctx.author.name}", auto_archive_duration=60)
         await thread.add_user(ctx.author)
-        await thread.send(f'Thread de suporte criada para {ctx.author.mention}. Somente você e os administradores podem ver.')
+        
+        embed = discord.Embed(
+            title="📝 Ticket Criado",
+            description=f"Thread de suporte criada para {ctx.author.mention}. Somente você e os administradores podem ver.",
+            color=discord.Color.orange()
+        )
+        
+        await thread.send(embed=embed)
 
     @bot.command(name="clear", help="Limpa uma quantidade específica de mensagens.")
     @commands.has_permissions(manage_messages=True)
     async def clear(ctx, amount: int):
         if amount <= 0:
-            await ctx.send("Por favor, especifique um número positivo de mensagens para apagar.")
+            embed = discord.Embed(
+                title="Erro",
+                description="Por favor, especifique um número positivo de mensagens para apagar.",
+                color=discord.Color.red()
+            )
+            await ctx.send(embed=embed)
             return
         deleted = await ctx.channel.purge(limit=amount)
-        await ctx.send(f"{len(deleted)} mensagens apagadas.", delete_after=5)
+        
+        embed = discord.Embed(
+            title="🧹 Limpeza",
+            description=f"{len(deleted)} mensagens apagadas.",
+            color=discord.Color.green()
+        )
+        await ctx.send(embed=embed, delete_after=5)
 
     @bot.command(name="greet")
     @commands.has_permissions(administrator=True)
     async def set_greet_channel(ctx):
         save_greet_channel(ctx.channel.id)
-        await ctx.send(f"Canal de boas-vindas definido para {ctx.channel.mention}.")
+        
+        embed = discord.Embed(
+            title="📢 Canal de Boas-vindas Definido",
+            description=f"Canal de boas-vindas definido para {ctx.channel.mention}.",
+            color=discord.Color.blue()
+        )
+        await ctx.send(embed=embed)
 
     @bot.event
     async def on_member_join(member):
